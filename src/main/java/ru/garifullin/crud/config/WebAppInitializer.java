9ff2a4173fa5082a7218f25c8f4c6908deea02e9
GarifullinII <ildaridrisovich@gmail.com>
2026-02-10 14:25:02 +0400

    Initial commit

---
+++ src/main/java/ru/garifullin/crud/config/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package ru.garifullin.crud.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+
+        AnnotationConfigWebApplicationContext context =
+                new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class);
+
+        DispatcherServlet dispatcherServlet =
+                new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
+
---
+++ src/main/java/ru/garifullin/crud/controllers/PeopleController.java
@@ -0,0 +1,5 @@
+package ru.garifullin.crud.controllers;
+
+public class PeopleController {
+
+}
