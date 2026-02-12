ae41d2ac89c619a0451c862dc95163bc2956aa2f
GarifullinII <ildaridrisovich@gmail.com>
2026-02-12 17:01:26 +0400

    add Patch, Delete

--- src/main/java/ru/garifullin/crud/config/WebAppInitializer.java
+++ src/main/java/ru/garifullin/crud/config/WebAppInitializer.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebAppInitializer implements WebApplicationInitializer {
@@ -22,6 +23,15 @@ public class WebAppInitializer implements WebApplicationInitializer {
 
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext.addFilter(
+                "hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()
+        ).addMappingForUrlPatterns(null, true, "/*");
     }
 }
 
